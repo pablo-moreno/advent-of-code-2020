a082020bd3481897fdd335ce11d1c35c1fda14b6
Name <pablomoreno.inf@gmail.com>
2020-12-02 17:33:53 +0100

    Day 2

---
+++ day_two/main.py
@@ -0,0 +1,71 @@
+def get_input(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+
+
+class DayTwo(object):
+    @staticmethod
+    def parse_line(line: str) -> (str, str, str):
+        constraint_str, password = line.split(':')
+        letter_range, letter = constraint_str.split(' ')
+
+        return letter, letter_range, password.strip()
+
+    @staticmethod
+    def validate_password_first_part(letter: str, letter_range: str, password: str) -> bool:
+        min_chars, max_chars = [int(x) for x in letter_range.split('-')]
+        count = 0
+
+        for char in password:
+            if char == letter:
+                count += 1
+
+        return min_chars <= count <= max_chars
+
+    @staticmethod
+    def validate_password_second_part(letter: str, letter_range: str, password: str) -> bool:
+        valid_positions = {int(x) for x in letter_range.split('-')}
+        positions = set()
+
+        for i, char in enumerate(password, start=1):
+            if char == letter:
+                positions.add(i)
+
+        return len(positions.intersection(valid_positions)) == 1
+
+    @staticmethod
+    def solve_first_part(lines):
+        valid_passwords = 0
+
+        for line in lines:
+            letter, letter_range, password = DayTwo.parse_line(line)
+
+            if DayTwo.validate_password_first_part(letter, letter_range, password):
+                valid_passwords += 1
+
+        return valid_passwords
+
+    @staticmethod
+    def solve_second_part(lines):
+        valid_passwords = 0
+
+        for line in lines:
+            letter, letter_range, password = DayTwo.parse_line(line)
+
+            if DayTwo.validate_password_second_part(letter, letter_range, password):
+                valid_passwords += 1
+
+        return valid_passwords
+
+
+def main():
+    lines = get_input('inputs/day_2.txt')
+    result = DayTwo.solve_first_part(lines)
+    print(f'The result of the first puzzle is {result}')
+
+    result = DayTwo.solve_second_part(lines)
+    print(f'The result of the second puzzle is {result}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_day_two.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from ..day_two import DayTwo
+
+
+class DayOneTestCase(TestCase):
+    def test_first_part_valid_passwords(self):
+        line = '1-3 a: abcdef'
+        letter, letter_range, password = DayTwo.parse_line(line)
+        result = DayTwo.validate_password_first_part(letter, letter_range, password)
+
+        assert result is True
+
+        line = '2-9 c: ccccccccc'
+        letter, letter_range, password = DayTwo.parse_line(line)
+        result = DayTwo.validate_password_first_part(letter, letter_range, password)
+
+        assert result is True
+
+    def test_first_part_invalid_password(self):
+        line = '1-3 a: cdefg'
+        letter, letter_range, password = DayTwo.parse_line(line)
+        result = DayTwo.validate_password_first_part(letter, letter_range, password)
+
+        assert result is False
+
+    def test_second_part_valid_password(self):
+        line = '1-3 a: abcde'
+        letter, letter_range, password = DayTwo.parse_line(line)
+        result = DayTwo.validate_password_second_part(letter, letter_range, password)
+
+        assert result is True
+
+    def test_second_part_invalid_passwords(self):
+        line = '1-3 a: cdefg'
+        letter, letter_range, password = DayTwo.parse_line(line)
+        result = DayTwo.validate_password_second_part(letter, letter_range, password)
+
+        assert result is False
+
+        line = '2-9 c: ccccccccc'
+        letter, letter_range, password = DayTwo.parse_line(line)
+        result = DayTwo.validate_password_second_part(letter, letter_range, password)
+
+        assert result is False
